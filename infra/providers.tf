d98161c384ee72acce1089e8ba0691fc8744cdec
Igor Iwashita de Oliveira <igor.iwashita@accenture.com>
2023-07-01 22:32:28 -0300

    s3

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-files"
+    key    = "tf/config"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -6,7 +11,7 @@ terraform {
     }
   }
 }
-
+  
 provider "aws" {
   region = "us-east-1"
 }
